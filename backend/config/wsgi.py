75bc4b777d55ee403aeee2be936e583d7e4feae9
H2SO4-1191 <H2SO4.1191@gmail.com>
2025-11-15 12:56:35 +0300

    Fixed a minor error with the model's path on the VM

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
